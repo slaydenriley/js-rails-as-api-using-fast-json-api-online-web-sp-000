a496d34c43751057c087158af57df383b1a14f0c
Riley Slayden <slaydenriley@gmail.com>
2020-08-04 17:02:27 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,9 +1,9 @@
 class SightingsController < ApplicationController
   def show
-  sighting = Sighting.find_by(id: params[:id])
-  options = {
-    include: [:bird, :location]
-  }
-  render json: SightingSerializer.new(sighting, options)
-end
+    sighting = Sighting.find_by(id: params[:id])
+    options = {
+      include: [:bird, :location]
+    }
+    render json: SightingSerializer.new(sighting, options)
+  end
 end
